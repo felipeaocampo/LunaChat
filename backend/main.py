7533312e89ac6bac063056250aa8069cecef4eb6
Felipe Ocampo <felipe.aocampo@gmail.com>
2023-07-01 19:03:55 -0700

    done with backend

--- backend/main.py
+++ backend/main.py
@@ -11,6 +11,8 @@ import openai
 
 # Custom Function Imports
 from functions.openai_requests import convert_audio_to_text, get_chat_response
+from functions.database import store_messages, reset_messages
+from functions.text_to_speech import convert_text_to_speech
 
 
 
@@ -38,12 +40,25 @@ app.add_middleware(
 async def check_health():
     return {"message": "Healthy"}
 
+# Reset Messages
+@app.get("/reset")
+async def reset_conversation():
+    reset_messages()
+    return {"message": "conversation reset"}
+
 
 # Get audio 
-@app.get("/post-audio-get/")
-async def get_audio():
+@app.post("/post-audio/")
+async def post_audio(file: UploadFile = File(...)):
+    
+    # This code was for set up purposes. Not for the actual app
     #  Get saved audio
-    audio_input = open("voice.mp3", "rb")
+    # audio_input = open("voice.mp3", "rb")
+
+    # Save file from frontend
+    with open(file.filename, "wb") as buffer:
+        buffer.write(file.file.read())
+    audio_input = open(file.filename, "rb")
 
     # Decode Audio
     message_decoded = convert_audio_to_text(audio_input)
@@ -54,10 +69,28 @@ async def get_audio():
     
     # Get ChatGPT Reponse
     chat_response = get_chat_response(message_decoded)
-
     print(chat_response)
 
-    return "Done"
+    #  Guard: Ensure text chat response was received
+    if not chat_response:
+        return HTTPException(status_code=400, detail="Failed to chat response")
+
+    # Convert chat response to audio
+    audio_output = convert_text_to_speech(chat_response)
+
+    #  Guard: Ensure audio response was received
+    if not audio_output:
+        return HTTPException(status_code=400, detail="Failed to get Eleven Labs audio response")
+
+    # Store messages
+    store_messages(message_decoded, chat_response)
+
+    # Create a generator that yields chuncks of data
+    def iterfile():
+        yield audio_output
+
+    # Return audio file
+    return StreamingResponse(iterfile(), media_type="application/octet-stream")
 
 
 # Post bot response
